147d0667b90a8241ed7a7f24c881c08aca61c233
Lalit Kumar <lalitbgs2584@gmail.com>
2026-02-17 20:30:06 +0530

    Minor changes

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   transpilePackages: ["@workspace/ui", "@repo/auth", "@repo/db"],
   output: "standalone",
+  serverExternalPackages: ["@prisma/client","@prisma/adapter-pg"]
 };
 
 export default nextConfig;
